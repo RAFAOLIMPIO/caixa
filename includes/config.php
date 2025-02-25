a19b085816ae08fbdcdd1da1e78b985c3bea686c
RAFAEL OLIMPIO <165104691+RAFAOLIMPIO@users.noreply.github.com>
2025-02-25 06:47:53 -0300

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,17 +1,17 @@
 <?php
 session_start();
 
-// Conexão com o banco (CORRIGIDO)
-$host = 'localhost';
-$user = 'root';
-$password = ''; // Antes estava $pass
-$database = 'sistema_loja'; // Antes estava $dbname
+// Conexão com o banco (AWS RDS)
+$host = 'sistema-caixa.cpqiw2qka4z1.sa-east-1.rds.amazonaws.com'; // Endpoint do RDS
+$user = 'admin'; // Nome de usuário do RDS
+$password = 'Melancia09'; // Senha do RDS
+$database = 'sistema-caixa'; // Nome do banco de dados
 
 try {
     $pdo = new PDO(
         "mysql:host=$host;dbname=$database;charset=utf8mb4", 
         $user, 
-        $password // Usar $password que foi definida
+        $password
     );
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
