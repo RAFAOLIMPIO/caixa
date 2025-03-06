dea32a14312be467ec3a1ccdb7746559f907e039
RAFAEL OLIMPIO <165104691+RAFAOLIMPIO@users.noreply.github.com>
2025-03-06 16:57:28 -0300

    Atualizar o index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $erro = '';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
