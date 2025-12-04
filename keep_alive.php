a1c3228c49dcb8b953f530d27cc770117658b85b
Seu Nome <seuemail@exemplo.com>
2025-12-04 00:19:08 -0300

    Reiniciando repositório com novo projeto

--- keep_alive.php
+++
@@ -1,6 +0,0 @@
-<?php
-// keep_alive.php - usado para manter sessão ativa via AJAX
-include __DIR__ . '/includes/config.php';
-http_response_code(200);
-echo json_encode(['ok' => true, 'time' => time()]);
-?>
