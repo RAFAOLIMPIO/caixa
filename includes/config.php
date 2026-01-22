cabcffd6b1204c82d4c7b281fee5c4bbb7d91492
RAFAEL OLIMPIO <165104691+RAFAOLIMPIO@users.noreply.github.com>
2026-01-21 21:07:58 -0300

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -10,7 +10,6 @@ if (session_status() === PHP_SESSION_NONE) {
 ob_start();
 
 // Configurações de conexão com o banco Render PostgreSQL
-// Configurações de conexão com o banco Render PostgreSQL
 define('DB_HOST', 'dpg-d5o45nmid0rc739178kg-a.oregon-postgres.render.com');
 define('DB_PORT', '5432');
 define('DB_NAME', 'autogest_db');
