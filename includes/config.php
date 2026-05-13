b32cfa97368cbf8fea3e41f3617735b90e0cfcd0
RAFAEL OLIMPIO <165104691+RAFAOLIMPIO@users.noreply.github.com>
2026-05-13 11:00:08 -0300

    Atualizar o config.php

--- includes/config.php
+++ includes/config.php
@@ -54,6 +54,8 @@ try {
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false,
+            PDO::ATTR_PERSISTENT         => true,
+            PDO::ATTR_TIMEOUT            => 10,
         ]
     );
 } catch (PDOException $e) {
